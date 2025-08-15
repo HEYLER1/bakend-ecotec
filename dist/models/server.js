10cb4e3e96e8f442704d496381998fbb563fe2a7
jayler10 <jayler10jobs@gmail.com>
2025-08-14 22:10:15 -0400

    Primer commit

---
+++ dist/models/server.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const product_1 = __importDefault(require("../routes/product"));
+const user_1 = __importDefault(require("../routes/user"));
+const product_2 = require("./product");
+const user_2 = require("./user");
+class Server {
+    app;
+    port;
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.port = process.env.PORT || '3001';
+        this.listen();
+        this.midlewares();
+        this.routes();
+        this.dbConnect();
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Aplicacion corriendo en el puerto ' + this.port);
+        });
+    }
+    routes() {
+        this.app.use('/api/products', product_1.default);
+        this.app.use('/api/users', user_1.default);
+    }
+    midlewares() {
+        // Parseo body
+        this.app.use(express_1.default.json());
+        // Cors
+        this.app.use((0, cors_1.default)());
+    }
+    async dbConnect() {
+        try {
+            await product_2.Product.sync();
+            await user_2.User.sync();
+        }
+        catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    }
+}
+exports.default = Server;
+//# sourceMappingURL=server.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Server } from "./models/server";
+import dotenv from 'dotenv'
+dotenv.config();
+const servidor = new Server();
